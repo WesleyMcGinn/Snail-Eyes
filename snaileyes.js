db1b128a48eddd00b9c338d670e476cd7fe02915
Wesley <wesleymcginn1@gmail.com>
2025-07-11 11:21:08 -0400

    Create more adequate test

--- snaileyes.js
+++ snaileyes.js
@@ -38,14 +38,16 @@ var eyes = {
   },
   max : function() { // Returns the slot in the 3D environment that holds the maximum density out of all un-blobbed slots
     this.v3d[16777216] = 0; // Clear GOOSE
-    return this.v3d.reduce((max, current) => Math.max(max, current), 0);;
+    return this.v3d.reduce((max, current) => Math.max(max, current), 0);
   }
 }
 
 console.log("Prepping...");
 eyes.prep();
 console.log("Setting...");
-eyes.addPix(0,0,0,0,0);
+for (var i=0; i<64*48; i++) {
+  eyes.addPix(Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),i%64,Math.floor(i/64),'l');
+}
 console.log("Getting max...");
 var Max = eyes.max(); // Should be equal to spread value
 console.log(Max);
